13acc6929e9e0a4cf292ff5eb0a90b5cc90e0f68
Diovani Stefanello Facco <93200912+diovani-f@users.noreply.github.com>
2023-09-07 15:42:00 -0300

    touch and mouse

--- assinatura.js
+++ assinatura.js
@@ -1,42 +1,71 @@
 var canvas = document.getElementById("assinaturaCanvas");
-        var ctx = canvas.getContext("2d");
-        var assinaturaImagem = document.getElementById("assinaturaImagem");
+var ctx = canvas.getContext("2d");
+var assinaturaImagem = document.getElementById("assinaturaImagem");
 
-        var desenhando = false;
-        var xAnterior = 0;
-        var yAnterior = 0;
+var desenhando = false;
+var xAnterior = 0;
+var yAnterior = 0;
 
-        // Evento de mouse pressionado
-        canvas.addEventListener("mousedown", function (e) {
-            desenhando = true;
-            xAnterior = e.clientX - canvas.getBoundingClientRect().left;
-            yAnterior = e.clientY - canvas.getBoundingClientRect().top;
-        });
+// Evento de mouse pressionado
+canvas.addEventListener("mousedown", function (e) {
+    desenhando = true;
+    xAnterior = e.clientX - canvas.getBoundingClientRect().left;
+    yAnterior = e.clientY - canvas.getBoundingClientRect().top;
+});
 
-        // Evento de mouse movido
-        canvas.addEventListener("mousemove", function (e) {
-            if (!desenhando) return;
-            var x = e.clientX - canvas.getBoundingClientRect().left;
-            var y = e.clientY - canvas.getBoundingClientRect().top;
+// Evento de toque pressionado
+canvas.addEventListener("touchstart", function (e) {
+    desenhando = true;
+    xAnterior = e.touches[0].clientX - canvas.getBoundingClientRect().left;
+    yAnterior = e.touches[0].clientY - canvas.getBoundingClientRect().top;
+});
 
-            ctx.beginPath();
-            ctx.moveTo(xAnterior, yAnterior);
-            ctx.lineTo(x, y);
-            ctx.strokeStyle = "#000";
-            ctx.lineWidth = 2;
-            ctx.stroke();
+// Evento de mouse movido
+canvas.addEventListener("mousemove", function (e) {
+    if (!desenhando) return;
+    var x = e.clientX - canvas.getBoundingClientRect().left;
+    var y = e.clientY - canvas.getBoundingClientRect().top;
 
-            xAnterior = x;
-            yAnterior = y;
-        });
+    ctx.beginPath();
+    ctx.moveTo(xAnterior, yAnterior);
+    ctx.lineTo(x, y);
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 2;
+    ctx.stroke();
 
-        // Evento de mouse liberado
-        canvas.addEventListener("mouseup", function () {
-            desenhando = false;
-        });
+    xAnterior = x;
+    yAnterior = y;
+});
 
-        // Limpar a assinatura
-        function limparAssinatura() {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            assinaturaImagem.src = "";
-        }
+// Evento de toque movido
+canvas.addEventListener("touchmove", function (e) {
+    if (!desenhando) return;
+    var x = e.touches[0].clientX - canvas.getBoundingClientRect().left;
+    var y = e.touches[0].clientY - canvas.getBoundingClientRect().top;
+
+    ctx.beginPath();
+    ctx.moveTo(xAnterior, yAnterior);
+    ctx.lineTo(x, y);
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    xAnterior = x;
+    yAnterior = y;
+});
+
+// Evento de mouse liberado
+canvas.addEventListener("mouseup", function () {
+    desenhando = false;
+});
+
+// Evento de toque liberado
+canvas.addEventListener("touchend", function () {
+    desenhando = false;
+});
+
+// Limpar a assinatura
+function limparAssinatura() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    assinaturaImagem.src = "";
+}
